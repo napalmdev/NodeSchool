129b5e386df8eb53b551146f23504f88c437b6d7
Denis Nunes <denis_tecnosoft@hotmail.com>
2016-06-03 13:30:19 -0300

    Exercicio de State Resolvido

--- learYouReact/views/index.jsx
+++ learYouReact/views/index.jsx
@@ -30,9 +30,22 @@ class TodoList extends React.Component {
 
 
 class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {checked: false};
+  }
+
+  handleChange(e) {
+    this.setState({checked: e.target.checked});
+  }
+
+
   render() {
     return (
       <tr>
+        <td style={{border: "1px solid black"}}>
+          <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+        </td>
         <td style={{border: "1px solid black"}}>{this.props.title}</td>
         <td style={{border: "1px solid black"}}>{this.props.children}</td>
       </tr>
@@ -42,6 +55,7 @@ class Todo extends React.Component {
 Todo.propTypes = {
   title: React.PropTypes.string.isRequired
 };
+
 
 class TodoForm extends React.Component {
   render() {
