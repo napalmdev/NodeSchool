41db3a17b0e8dd2cd84e96e293ddc2ba779b2089
Denis Nunes <denis_tecnosoft@hotmail.com>
2016-06-06 08:31:45 -0300

    Primeiro exemplo com Style no React

--- learYouReact/views/index.jsx
+++ learYouReact/views/index.jsx
@@ -43,11 +43,11 @@ class Todo extends React.Component {
   render() {
     return (
       <tr>
-        <td style={{border: "1px solid black"}}>
+        <td style={style.tableContent}>
           <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
         </td>
-        <td style={{border: "1px solid black"}}>{this.props.title}</td>
-        <td style={{border: "1px solid black"}}>{this.props.children}</td>
+        <td style={style.tableContent}>{this.props.title}</td>
+        <td style={style.tableContent}>{this.props.children}</td>
       </tr>
     );
   }
@@ -66,3 +66,10 @@ class TodoForm extends React.Component {
     );
   }
 }
+
+
+let style = {
+  tableContent: {
+    border: "1px solid black"
+  }
+};
